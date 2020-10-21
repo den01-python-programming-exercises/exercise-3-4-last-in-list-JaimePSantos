b4d3ed5d2f027f009f545d1e3ead4b599ad9f21a
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-21 20:24:57 +0000

    solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #write your code below this line
+    strList = []
+    while(True):
+      str = input()
+      if(str==""):
+        break
+      strList.append(str)
+    print(strList[len(strList)-1])
 
 if __name__ == '__main__':
     main()
